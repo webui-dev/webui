52d3530c8148d5f701bf9de44457d9eb8447243a
Mehmet Ali Şipi <mehmetalisipi@gmail.com>
2023-04-23 20:16:45 +0300

    Fix Buffer_Overflow bug

--- src/webui.c
+++ src/webui.c
@@ -370,9 +370,9 @@ bool webui_script(void* window, const char* script, unsigned int timeout_second,
         if(buffer != NULL && buffer_length > 1) {
 
             // Copy response to the user's response buffer
-            size_t response_len = strlen(_webui_core.run_responses[run_id]);
+            size_t response_len = strlen(_webui_core.run_responses[run_id])+1;
             size_t bytes_to_cpy = (response_len <= buffer_length ? response_len : buffer_length);
-            snprintf(buffer, bytes_to_cpy+1, "%s", _webui_core.run_responses[run_id]);
+            snprintf(buffer, bytes_to_cpy, "%s", _webui_core.run_responses[run_id]);
         }
 
         _webui_free_mem((void *)_webui_core.run_responses[run_id]);
