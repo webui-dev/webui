4877d1e4daecfb07925fdc9e06a136e7b33c9c2b
Julien Oculi <xdrz.jot.sr@gmail.com>
2023-07-06 12:55:52 +0200

    fix: :pencil2: update deprecated js mime type

--- src/webui.c
+++ src/webui.c
@@ -4431,7 +4431,7 @@ static int _webui_http_handler(struct mg_connection *conn, void *_win) {
             // Send
             _webui_http_send(
                 conn, // 200
-                "text/javascript",
+                "application/javascript",
                 js
             );
 
@@ -4484,7 +4484,7 @@ static int _webui_http_handler(struct mg_connection *conn, void *_win) {
                         html = (char*) _webui_malloc(len);
                         if(win->html != NULL && js != NULL) {
                             sprintf(html, 
-                                "%s \n <script type = \"text/javascript\"> \n %s \n </script>",
+                                "%s \n <script type = \"application/javascript\"> \n %s \n </script>",
                                 win->html, js
                             );
                         }
