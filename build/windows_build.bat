b38c9eb532ca0f130790f7f44084c57dc45a7dc5
Julien Oculi <xdrz.jot.sr@gmail.com>
2023-07-12 01:21:23 +0200

    build: :sparkles: update local build script to include esbuild process

--- build/windows_build.bat
+++ build/windows_build.bat
@@ -19,6 +19,11 @@ echo Compiler: MSVC, GCC and TCC
 Set RootPath=%CD%\..\
 cd "%RootPath%"
 
+REM Transpiling TS to JS
+echo Transpile and bundle TS sources to webui.js
+cd "%RootPath%"
+esbuild --bundle --target="chrome90,firefox90,safari15" --format=esm --outdir=.\src\client .\src\client\webui.ts
+
 REM Converting JS source to C-String using xxd
 echo Converting JS source to C-String using xxd
 cd "%RootPath%"
