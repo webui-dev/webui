4919613bee8f95ddc5dd825881c5faa665f2e4e9
Julien Oculi <xdrz.jot.sr@gmail.com>
2023-07-12 02:01:00 +0200

    build: :bug: patch windows local build script

--- build/windows_build.bat
+++ build/windows_build.bat
@@ -22,7 +22,7 @@ cd "%RootPath%"
 REM Transpiling TS to JS
 echo Transpile and bundle TS sources to webui.js
 cd "%RootPath%"
-esbuild --bundle --target="chrome90,firefox90,safari15" --format=esm --outdir=.\src\client .\src\client\webui.ts
+cmd /c esbuild --bundle --target="chrome90,firefox90,safari15" --format=esm --log-level=silent --outdir=.\src\client .\src\client\webui.ts
 
 REM Converting JS source to C-String using xxd
 echo Converting JS source to C-String using xxd
