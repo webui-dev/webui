ffb33ba8317fc40dfa8902a3d9a59280ba282256
Hassan DRAGA <dragahassan@gmail.com>
2023-09-22 10:18:02 -0400

    Update ChromiumBased
    - Moving `ChromiumBased` to `user specified browser` section
    - Update the order of `ChromiumBased`

--- src/webui.c
+++ src/webui.c
@@ -3921,22 +3921,16 @@ static bool _webui_browser_start(_webui_window_t* win, const char* address, size
     if(browser == AnyBrowser && _webui_core.current_browser != 0)
         browser = _webui_core.current_browser;
 
-    // TODO: Convert address from [/...] to [file://...]
+    // #1 - Chrome - Works perfectly
+    // #2 - Edge - Works perfectly like Chrome
+    // #3 - Epic - Works perfectly like Chrome
+    // #4 - Vivaldi - Works perfectly like Chrome
+    // #5 - Brave - Shows a policy notification in the first run
+    // #6 - Firefox - Does not support App-Mode like Chrome (Looks not great)
+    // #7 - Yandex - Shows a big welcome window in the first run
+    // #8 - Chromium - Some Anti-Malware shows a false alert when using ungoogled-chromium-binaries
 
-    if(browser == ChromiumBased) {
-
-        // Open the window using a Chromium based browser
-
-        if(!_webui_browser_start_chromium(win, address))
-            if(!_webui_browser_start_chrome(win, address))
-                if(!_webui_browser_start_edge(win, address))
-                    if(!_webui_browser_start_brave(win, address))
-                        if(!_webui_browser_start_vivaldi(win, address))
-                            if(!_webui_browser_start_epic(win, address))
-                                if(!_webui_browser_start_yandex(win, address))
-                                    return false;
-    }
-    else if(browser != 0) {
+    if(browser != 0) {
 
         // Open the window using the user specified browser
 
@@ -3956,6 +3950,19 @@ static bool _webui_browser_start(_webui_window_t* win, const char* address, size
             return _webui_browser_start_yandex(win, address);
         else if(browser == Chromium)
             return _webui_browser_start_chromium(win, address);
+	else if(browser == ChromiumBased) {
+
+            // Open the window using a Chromium-based browser
+
+            if(!_webui_browser_start_chrome(win, address))
+                if(!_webui_browser_start_edge(win, address))
+                    if(!_webui_browser_start_epic(win, address))
+                        if(!_webui_browser_start_vivaldi(win, address))
+                            if(!_webui_browser_start_brave(win, address))
+                                if(!_webui_browser_start_yandex(win, address))
+                                    if(!_webui_browser_start_chromium(win, address))
+                                        return false;
+        }
         else
             return false;
     }
@@ -3985,15 +3992,6 @@ static bool _webui_browser_start(_webui_window_t* win, const char* address, size
     else {
 
         // Open the window using the default OS browser
-
-        // #1 - Chrome - Works perfectly
-        // #2 - Edge - Works perfectly like Chrome
-        // #3 - Epic - Works perfectly like Chrome
-        // #4 - Vivaldi - Works perfectly like Chrome
-        // #5 - Brave - Shows a policy notification in the first run
-        // #6 - Firefox - Does not support App-Mode like Chrome (Looks not great)
-        // #7 - Yandex - Shows a big welcome window in the first run
-        // #8 - Chromium - Some Anti-Malware shows a false alert when using ungoogled-chromium-binaries
 
         #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
             // Windows
