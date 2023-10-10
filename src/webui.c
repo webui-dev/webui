5c5ff7572596e79d38f4d1f6077312089791166a
Hassan DRAGA <dragahassan@gmail.com>
2023-10-10 18:02:58 -0400

    Fix WEBUI_MAX_ARG

--- src/webui.c
+++ src/webui.c
@@ -83,8 +83,8 @@ typedef struct _webui_timer_t {
 
 // Event data
 typedef struct webui_event_inf_t {
-	char* event_data[WEBUI_MAX_ARG];  // Event data (string | num | bool | raw)
-	size_t event_size[WEBUI_MAX_ARG]; // Event data size (in bytes)
+	char* event_data[WEBUI_MAX_ARG + 1];  // Event data (string | num | bool | raw)
+	size_t event_size[WEBUI_MAX_ARG + 1]; // Event data size (in bytes)
 	char* response;                   // Event response (string)
 } webui_event_inf_t;
 
