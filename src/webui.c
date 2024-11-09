f161bcaced7fd699a5e6ca56144202227307d248
kbkpbot <kbkpbot@sina.com>
2024-11-09 16:30:57 +0800

    make sure 4096 bytes not alloc 8192 bytes (#516)

--- src/webui.c
+++ src/webui.c
@@ -3755,7 +3755,7 @@ static size_t _webui_mb(size_t size) {
     size--;
 
     size_t block_size = 4;
-    while(block_size <= size)
+    while(block_size < size)
         block_size *= 2;
 
     return block_size;
