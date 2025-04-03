51a3d011203b3f49db5b140dedb363ae0dcde268
Albert <comsystemsuser2@gmail.com>
2025-04-03 17:03:02 -0400

    Update Bridge Build Scripts

--- bridge/build.bat
+++ bridge/build.bat
@@ -6,22 +6,6 @@ cd %~dp0
 FOR /F "tokens=*" %%i IN ('git rev-parse --show-toplevel') DO SET project_root=%%i
 cd %project_root%/bridge
 
-REM Check which python command is available
-    SET python_cmd=python
-    @REM python3 --version > NUL 2>&1
-    @REM IF %errorlevel%==0 (
-    @REM     SET python_cmd=python3
-    @REM ) ELSE (
-    @REM     python --version > NUL 2>&1
-    @REM     IF %errorlevel%==0 (
-    @REM         SET python_cmd=python
-    @REM     ) ELSE (
-    @REM         echo Error: Please install Python.
-    @REM         cd %cd%
-    @REM         exit /b
-    @REM     )
-    @REM )
-
 REM Check if node_modules\esbuild exists, if not, install using npm
 IF NOT EXIST "%project_root%\bridge\node_modules\esbuild\" (
     where npm > NUL 2>&1
@@ -37,7 +21,7 @@ IF NOT EXIST "%project_root%\bridge\node_modules\esbuild\" (
 
 REM Transpile WebUI-Bridge (TS to JS) & Convert WebUI-Bridge (JS to C)
 echo Transpile and bundle WebUI-Bridge from TypeScript to JavaScript...
-.\node_modules\.bin\esbuild --bundle --target="chrome90,firefox90,safari15" --format=esm --tree-shaking=false --minify-syntax --minify-whitespace --outdir=. ./webui.ts & %python_cmd% js2c.py
+.\node_modules\.bin\esbuild --bundle --target="chrome90,firefox90,safari15" --format=esm --tree-shaking=false --minify-syntax --minify-whitespace --outdir=. ./webui.ts & node js2c.js
 
 echo Done.
 cd %cd%
--- bridge/build.sh
+++ bridge/build.sh
@@ -12,26 +12,10 @@
 project_root=$(git rev-parse --show-toplevel)
 cd $project_root/bridge
 
-# Loop through all arguments
-for opt in "$@"; do
-	case $opt in
-		--silent)
-			silent=true
-			;;
-		*)
-			echo "Invalid option: $opt"
-			exit 0
-			;;
-	esac
-	shift  # Move to the next argument
-done
+echo "Transpile and bundle TS sources to webui.js"
+esbuild --bundle --target="chrome90,firefox90,safari15" --format=esm --tree-shaking=false --minify-syntax --minify-whitespace --outdir=. ./webui.ts
 
-if [ "$silent" = true ]; then log_level=--log-level=warning; fi
+echo "Convert JS source to C header"
+node js2c.js
 
-if [ "$silent" != true ]; then echo "Transpile and bundle TS sources to webui.js"; fi
-esbuild --bundle --target="chrome90,firefox90,safari15" --format=esm --tree-shaking=false --minify-syntax --minify-whitespace --outdir=. ./webui.ts $log_level
-
-if [ "$silent" != true ]; then echo "Convert JS source to C header"; fi
-python3 js2c.py
-
-if [ "$silent" != true ]; then echo "Done."; fi
+echo "Done."
